98b33fcbcbb53c785bdb8e40dddcff80d47e964f
Purukitto <purukitto@gmail.com>
2023-10-08 15:07:38 +0530

    Up new rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
@@ -39,6 +38,6 @@ export default [
 			format: "umd",
 			name: "simpleDungeon",
 		},
-		plugins: [resolve(), typescript(), commonjs()],
+		plugins: [esbuild(), commonjs(), resolve()],
 	}),
 ];
